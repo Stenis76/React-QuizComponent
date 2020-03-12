e85494ffc6d8b50c2b67162dec57c39dafefb766
Stenis76 <stenqvist.dick@gmail.com>
2020-03-12 11:38:14 +0100

    2:8

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -8,7 +8,10 @@ class QuizQuestion extends Component {
           <p>// instruction text goes here</p>
         </section>
         <section className="buttons">
-          <ul>//quiz question button logic goes here</ul>
+          <ul>
+            //quiz question button logic goes here
+            <li>{this.props.quiz_question.answer_options[0]}</li>
+          </ul>
         </section>
       </main>
     );
